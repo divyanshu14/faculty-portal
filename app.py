acf3d479c0b141ba405edc828f1419d208c67478
divyanshu14 <iamavi53@gmail.com>
2019-11-12 04:40:35 +0530

    established basic postgres connection

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template, url_for
+import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_DATABASE"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD")
+    )
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+
+    # This is how SQL statements will be executed using cursor's execute function
+    cur.execute('SELECT version()')
+
+    db_version = cur.fetchone()
+    print(db_version)
+    cur.close()
+    conn.close()
     return render_template('index.html')
 
 if __name__ == "__main__":
